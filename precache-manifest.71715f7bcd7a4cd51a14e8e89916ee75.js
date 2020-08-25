9eec9c0a85613c024dc9ddcdd231db3706f552a9
luoxin <cgland@CGLanddeMBP.lan>
2020-08-25 11:58:39 +0800

    Updates

---
+++ precache-manifest.71715f7bcd7a4cd51a14e8e89916ee75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e84ed338e09fb4d3a578ac40f5b964",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "c88624928f14895246d6",
+    "url": "/react-tutorial/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fd5335e167cade81462b",
+    "url": "/react-tutorial/static/js/2.13a8129e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.13a8129e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c88624928f14895246d6",
+    "url": "/react-tutorial/static/js/main.d2a76cfc.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
