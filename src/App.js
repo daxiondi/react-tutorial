230b020def8262df56178bda758d77d34cd5da50
luoxin <cgland@CGLanddeMBP.lan>
2020-08-25 11:50:15 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Table from './Table';
+import Form from './Form';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    characters: [],
+  }
+
+  removeCharacter = (index) => {
+    const {characters} = this.state
+
+    this.setState({
+      characters: characters.filter( (characters, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+
+  handleSubmit = (character) => {
+    this.setState({characters: [...this.state.characters, character]})
 }
+
+  render() {
+  const name = 'Tania'
+  const heading = <h1>Hello, {name}</h1>
+
+  const { characters } = this.state
+
+  // const characters = [
+  //   {
+  //     name: 'Charlie',
+  //     job: 'Janitor',
+  //   },
+  //   {
+  //     name: 'Mac',
+  //     job: 'Bouncer',
+  //   },
+  //   {
+  //     name: 'Dee',
+  //     job: 'Aspring actress',
+  //   },
+  //   {
+  //     name: 'Dennis',
+  //     job: 'Bartender',
+  //   },
+  // ]
+
+  const table = <div className="container" characterData={characters}><Table/></div>
+
+  return (
+    <div className="container">
+    <Table characterData={characters} removeCharacter={this.removeCharacter} />
+    <h3>Add New</h3>
+    <Form handleSubmit={this.handleSubmit}/>
+  </div>
+  )
+
+  }
+}
+
+// function App() {
+//   const name = 'Tania'
+//   const heading = <h1>Hello, {name}</h1>
+
+//   const characters = [
+//     {
+//       name: 'Charlie',
+//       job: 'Janitor',
+//     },
+//     {
+//       name: 'Mac',
+//       job: 'Bouncer',
+//     },
+//     {
+//       name: 'Dee',
+//       job: 'Aspring actress',
+//     },
+//     {
+//       name: 'Dennis',
+//       job: 'Bartender',
+//     },
+//   ]
+
+//   const table = <div className="container"><Table characterData={characters} /></div>
+
+//   return (
+//     table
+
+//   )
+// }
 
 export default App;
